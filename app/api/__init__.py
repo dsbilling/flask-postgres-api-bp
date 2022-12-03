bbc856892339900a8125cdb5a5fb10c4f42d18ae
Daniel S. Billing <daniel@izy.no>
2022-12-03 17:21:28 +0100

    Added default error handler

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import Blueprint
 from flask_restx import Api
 from app.api.users.routes import api as users_ns
@@ -7,3 +9,10 @@ from config import Config
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp, title=Config.SWAGGER_TITLE, description=Config.SWAGGER_DESCRIPTION, version=Config.SWAGGER_VERSION)
 api.add_namespace(users_ns)
+
+
+@api.errorhandler(Exception)
+def default_error_handler(e):
+    logging.exception(e)
+    message = 'An unhandled exception occurred.'
+    return {'message': message}, 500
