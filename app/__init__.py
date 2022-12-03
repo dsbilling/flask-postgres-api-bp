d0231e854baeb798a71da52ce9756b43445b6fae
Daniel S. Billing <daniel@izy.no>
2022-12-03 17:03:17 +0100

    Reworked init of extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,15 @@
 from config import Config
 from flask import Flask
 from .api import api_bp
-import logging
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-
-database = SQLAlchemy()
-db_migration = Migrate()
+from .ext import db, migrate
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    register_extensions(app)
-    logging.basicConfig(level=logging.DEBUG)
-    app.register_blueprint(api_bp, url_prefix='/')
-    return app
-
-
-def register_extensions(app):
-
-    db = SQLAlchemy()
-    migrate = Migrate()
-
     db.init_app(app)
     migrate.init_app(app, db)
+
+    app.register_blueprint(api_bp)
+    return app
