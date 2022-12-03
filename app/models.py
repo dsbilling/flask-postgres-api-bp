c8ab176fbb746c0069e764802f330bc1ad4ee948
Daniel S. Billing <daniel@izy.no>
2022-12-03 17:03:53 +0100

    Reworked the user model

--- app/models.py
+++ app/models.py
@@ -1,63 +1,34 @@
-from app import database
-from datetime import datetime
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
 from werkzeug.security import generate_password_hash, check_password_hash
+from .ext import db
+
+Model = db.Model
+Column = db.Column
 
 
-class User(database.Model):
+class User(Model):
     __tablename__ = 'users'
 
-    id = database.Column(database.UUID, primary_key=True)
-    name = database.Column(database.String(100))
-    email = database.Column(database.String(70), unique=True)
-    hash = database.Column(database.String(160))
-    created_at = database.Column(database.DateTime)
-    confirmed_at = database.Column(database.DateTime)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email = Column(db.String, unique=True, nullable=True)
+    hash = Column(db.String)
+    name = Column(db.String)
+    confirmed_at = Column(db.DateTime, nullable=True, default=None)
+    created_at = Column(db.DateTime)
+    updated_at = Column(db.DateTime)
+    deleted_at = Column(db.DateTime, nullable=True, default=None)
 
-    def __init__(self, name: str, email: str, password_plaintext: str, user_type='User'):
-        self.name = name
-        self.email = email
-        self.hash = self._generate_password_hash(password_plaintext)
-        self.created_at = datetime.now()
-        self.confirmed_at = datetime.now()
-        self.user_type = user_type
-
-    def is_password_correct(self, password_plaintext: str):
-        return check_password_hash(self.hash, password_plaintext)
-
-    def set_password(self, password_plaintext: str):
-        self.hash = self._generate_password_hash(password_plaintext)
-
-    @staticmethod
-    def _generate_password_hash(password_plaintext):
-        return generate_password_hash(password_plaintext)
-
-    def __repr__(self):
-        return f'<User: {self.hash}>'
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
 
     @property
-    def is_authenticated(self):
-        """Return True if the user has been successfully registered."""
-        return True
+    def password(self):
+        raise AttributeError('Password is protected')
 
-    @property
-    def is_active(self):
-        """Always True, as all users are active."""
-        return True
+    @password.setter
+    def password(self, password):
+        self.hash = generate_password_hash(password)
 
-    @property
-    def is_anonymous(self):
-        """Always False, as anonymous users aren't supported."""
-        return False
-
-    def get_id(self):
-        """Return the user ID as a unicode string (`str`)."""
-        return str(self.id)
-
-    def is_admin(self):
-        return self.user_type == 'Admin'
-
-    def confirm_email_address(self):
-        self.confirmed_at = datetime.now()
-
-    def remove_confirmation_email_address(self):
-        self.confirmed_at = None
+    def verify_password(self, password):
+        return check_password_hash(self.hash, password)
