09f8456a6134a3c85c15fcc8346479e12dbfd6cf
Daniel S. Billing <daniel@izy.no>
2022-12-20 10:15:02 +0100

    Updated users endpoint

--- app/api/users/routes.py
+++ app/api/users/routes.py
@@ -1,12 +1,17 @@
-from flask_restx import Resource
-from flask_restx import Namespace
+from flask import jsonify
+from flask_restx import Resource, Namespace
+from flask_sqlalchemy_session import current_session
+from app.models import User
 
 api = Namespace('users', description='People who use the app')
 
 
 @api.route('/')
 class Users(Resource):
-    @api.doc('list_users')
+    @api.response(200, 'Success')
     def get(self):
-        """Get all users"""
-        return {'message': 'Hello World!'}
+        users = current_session.session.query(User).all()
+        response_list = []
+        for row in users:
+            response_list.append(row.to_dict())
+        return jsonify(response_list)
